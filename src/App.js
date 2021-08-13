004903afe8ef7c53e48c28c584f757d723bad197
JongWoo Ha <jongwooha98@gmail.com>
2021-08-13 19:58:55 +0900

    ADD: react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import Header from './components/Header';
-import Newsfeed from './components/Newsfeed';
-import Stats from './components/Stats';
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
+import Nav from './components/Nav/Nav';
+import Newsfeed from './components/Newsfeed/Newsfeed';
+import Lists from './components/Lists/Lists';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      {/* Header */}
-      <div className="app__header">
-        <Header />
-      </div>
-      {/* Body */}
-      <div className="app__body">
-        <div className="app__container">
-          <Newsfeed />
-          <Stats />
-        </div>
-      </div>
+      <Router>
+        <header className="app__header">
+          <Nav />
+        </header>
+        <Switch>
+          <main className="app__body">
+            <div className="app__container">
+              <Route exact path="/" component={Newsfeed}>
+                <Newsfeed />
+                <Lists />
+              </Route>
+            </div>
+          </main>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
 export default App;
+{
+  /* <div className="App">
+     
+      <div className="app__header">
+        <Nav />
+      </div>
+   
+      <div className="app__body">
+        <div className="app__container">
+          <Newsfeed />
+          <Lists />
+        </div>
+      </div>
+  </div> */
+}
+
+{
+  /* <>
+<Router>
+  <header className="header" id="header">
+    <Nav />
+  </header>
+  <Switch>
+    <main className="main">
+      <Route exact path="/" component={Home}>
+        <Home />
+        <About />
+        <Skills />
+        <Qualification />
+        <Portfolio />
+        <Contact />
+      </Route>
+      <Route exact path="/projects" component={Projects} />
+    </main>
+  </Switch>
+  <Link to="#" className="scroll-up" id="scroll-up">
+    <i className="uil uil-arrow-up scroll-up__icon" />
+  </Link>
+  <footer className="footer">
+    <Footer />
+  </footer>
+</Router>
+</> */
+}
