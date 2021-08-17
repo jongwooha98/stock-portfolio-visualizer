29d59d281cad24b1d13cf2bf07f392e58487cc99
JongWoo Ha <jongwooha98@gmail.com>
2021-08-17 23:34:42 +0900

    ADD: Firestore CRUD functions

---
+++ src/firebase/Crud.js
@@ -0,0 +1,29 @@
+import firebase from './firebase';
+import { db } from './firebase';
+
+const myStocksDb = db.collection('/myStocks');
+
+const getAll = () => {
+  return myStocksDb;
+};
+
+const create = (data) => {
+  return myStocksDb.add(data);
+};
+
+const update = (id, value) => {
+  return myStocksDb.doc(id).update(value);
+};
+
+const remove = (id) => {
+  return myStocksDb.doc(id).delete();
+};
+
+const Crud = {
+  getAll,
+  create,
+  update,
+  remove,
+};
+
+export default Crud;
