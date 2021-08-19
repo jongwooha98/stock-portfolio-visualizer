cf243cf1c17552c75bf3a74fcd2be17b3f64aa76
JongWoo Ha <jongwooha98@gmail.com>
2021-08-20 01:32:52 +0900

    Update BottomNav.js

--- src/components/Nav/BottomNav.js
+++ src/components/Nav/BottomNav.js
@@ -4,10 +4,11 @@ import { ReactComponent as Logo } from '../../assets/images/toss-logo.svg';
 import { HashLink as Link } from 'react-router-hash-link';
 import { makeStyles } from '@material-ui/core/styles';
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import PieChartIcon from '@material-ui/icons/PieChart';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 import SmsIcon from '@material-ui/icons/Sms';
-import ShowChartIcon from '@material-ui/icons/ShowChart';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
 const useStyles = makeStyles({
   root: {
     position: 'fixed',
@@ -20,13 +21,15 @@ const useStyles = makeStyles({
     zIndex: '1000',
     boxShadow: '0 -1px 4px rgba(0, 0, 0, 0.15)',
   },
-  successIcon: {
-    color: 'green',
-    selected: 'blue',
+  nav: {
+    '&$selected': {
+      color: '#3456ff',
+    },
   },
-  errorIcon: {
-    color: 'red',
+  logo: {
+    paddingTop: '0 !important',
   },
+  selected: {},
 });
 
 function BottomNav() {
@@ -45,23 +48,44 @@ function BottomNav() {
         <BottomNavigationAction
           label="Portfolio"
           value="Portfolio"
-          icon={<ShowChartIcon className={classes.successIcon} />}
+          icon={<PieChartIcon />}
+          classes={{
+            root: classes.nav,
+            selected: classes.selected,
+          }}
         />
         <BottomNavigationAction
-          label="Watchlist"
-          value="Watchlist"
-          icon={<FavoriteIcon />}
+          label="Cash"
+          value="Cash"
+          icon={<AccountBalanceIcon />}
+          classes={{
+            root: classes.nav,
+            selected: classes.selected,
+          }}
         />
-        <BottomNavigationAction label="" value="" icon={<Logo />} />
+        <BottomNavigationAction
+          label=""
+          value=""
+          icon={<Logo />}
+          className={classes.logo}
+        />
         <BottomNavigationAction
           label="Messages"
           value="Messages"
           icon={<SmsIcon />}
+          classes={{
+            root: classes.nav,
+            selected: classes.selected,
+          }}
         />
         <BottomNavigationAction
           label="Account"
           value="Account"
           icon={<AccountCircleIcon />}
+          classes={{
+            root: classes.nav,
+            selected: classes.selected,
+          }}
         />
       </BottomNavigation>
     </div>
@@ -69,53 +93,3 @@ function BottomNav() {
 }
 
 export default BottomNav;
-
-// function Nav() {
-//   return (
-//     <nav className="nav container">
-//       <Link to="/#" className="nav__logo">
-//         <div className="header__logo">
-//           <img src={tossLogo} alt="" width={90} />
-//         </div>
-//       </Link>
-//       <div className="nav__menu">
-//         <ul className="nav__list grid">
-//           <li className="nav__item">
-//             <Link to="/#portfolio" className="nav__link active-link">
-//               <i className="uil uil-estate nav__icon" /> Portfolio
-//             </Link>
-//           </li>
-//           <li className="nav__item">
-//             <Link to="/#news" className="nav__link">
-//               <i className="uil uil-list-ol-alt nav__icon" /> News
-//             </Link>
-//           </li>
-//           <li className="nav__item">
-//             <Link to="/#watchlist" className="nav__link">
-//               <i className="uil uil-list-ol-alt nav__icon" /> Watchlist
-//             </Link>
-//           </li>
-//           <li className="nav__item">
-//             <Link to="/#account" className="nav__link">
-//               <i className="uil uil-list-ol-alt nav__icon" /> Account
-//             </Link>
-//           </li>
-//         </ul>
-//       </div>
-//       {/* search bar */}
-//       {/* <div className="header__search">
-//         <div className="header__search-container">
-//           <input placeholder="search" type="text" />
-//         </div>
-//       </div> */}
-
-//       <div className="nav__btns">
-//         <div className="nav__toggle" id="nav-toggle">
-//           <i className="uil uil-apps" />
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Nav;
