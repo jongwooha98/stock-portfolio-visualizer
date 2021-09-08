414eb9837105db2e4028a2e4bac17dd4ceac6a92
JongWoo Ha <jongwooha98@gmail.com>
2021-09-08 20:53:34 +0900

    Update Firestore CRUD
    get UID from Redux state

--- src/firebase/Crud.js
+++ src/firebase/Crud.js
@@ -1,21 +1,45 @@
 // import firebase from './firebase';
 import { db } from './firebase';
-
-const myStocksDb = db.collection('/myStocks');
+import store from '../redux/store';
+// import { useSelector } from 'react-redux';
 
 const getAll = () => {
+  const state = store.getState();
+  const currentUid = state.authStatus.uid;
+  const myStocksDb = db
+    .collection('users')
+    .doc(`${currentUid}`)
+    .collection('portfolio');
   return myStocksDb;
 };
 
 const create = (data) => {
+  const state = store.getState();
+  const currentUid = state.authStatus.uid;
+  const myStocksDb = db
+    .collection('users')
+    .doc(`${currentUid}`)
+    .collection('portfolio');
   return myStocksDb.add(data);
 };
 
 const update = (id, value) => {
+  const state = store.getState();
+  const currentUid = state.authStatus.uid;
+  const myStocksDb = db
+    .collection('users')
+    .doc(`${currentUid}`)
+    .collection('portfolio');
   return myStocksDb.doc(id).update(value);
 };
 
 const remove = (id) => {
+  const state = store.getState();
+  const currentUid = state.authStatus.uid;
+  const myStocksDb = db
+    .collection('users')
+    .doc(`${currentUid}`)
+    .collection('portfolio');
   return myStocksDb.doc(id).delete();
 };
 
@@ -25,5 +49,4 @@ const Crud = {
   update,
   remove,
 };
-
 export default Crud;
